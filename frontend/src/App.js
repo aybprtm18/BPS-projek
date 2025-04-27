41b1bb69ce1403f5bcef4ee014d95892b4923d76
aybprtm18 <ayubpratama018@gmail.com>
2025-04-27 20:15:26 +0700

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar";
 
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to BPS Project</h1>
+    </div>
+  );
+}
+
+export default App;
